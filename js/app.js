59778710b3876d42b77a912bd966ce84946629ab
anna mendoza <anna@curvecommunications.biz>
2016-06-19 16:57:45 -0700

    updated the last of the street-fighter challenge'

--- js/app.js
+++ js/app.js
@@ -10,18 +10,30 @@ $(document).ready ( function() {
 		$('.ryu-ready').hide();
 	})
 	.mousedown ( function() {
-		console.log("mousedown");
+		//console.log("mousedown");
+		playHadouken()
 		$('.ryu-still').hide();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-
+		$('.hadouken').finish().show().animate(
+			{'left' : '1020px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left' , '520px' );
+			}
+		);
 	})
 	.mouseup( function ()  {
-		console.log("mouseup");
+		//console.log("mouseup");
 		$('.ryu-ready').show();
 		$('.ryu-throwing').hide();
-		$('.hadouken').hide();
 	});
+
+	function playHadouken () {
+		$('#hadouken-sound')[0].volume = 0.5;
+		$('#hadouken-sound')[0].load();
+	 	$('#hadouken-sound')[0].play();
+	}
 	
 });
