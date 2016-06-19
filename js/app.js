a48d310f45364f384d87fa9d96d23afb730b64f9
anna mendoza <anna@curvecommunications.biz>
2016-06-19 16:28:17 -0700

    playing around with javascript

--- js/app.js
+++ js/app.js
@@ -1,13 +1,27 @@
-$(document).ready (function() {
+$(document).ready ( function() {
 	$('.ryu').hover(function() {
 		// alert('mouse entered .ryu div');
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
 	})
-	.mouseleave(function() {
+	.mouseleave( function() {
 		//alert('mouse left');
 		$('.ryu-still').show();
 		$('.ryu-ready').hide();
-	});
+	})
+	.mousedown ( function() {
+		console.log("mousedown");
+		$('.ryu-still').hide();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show();
 
+	})
+	.mouseup( function ()  {
+		console.log("mouseup");
+		$('.ryu-ready').show();
+		$('.ryu-throwing').hide();
+		$('.hadouken').hide();
+	});
+	
 });
